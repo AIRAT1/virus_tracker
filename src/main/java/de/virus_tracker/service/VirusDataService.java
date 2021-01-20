0e9d89fc5250b1be2e92236cd61620a4511c707f
Ayrat <ayrat1@mail.ru>
2021-01-20 22:21:12 +0300

    fetch data from server 10.27

---
+++ src/main/java/de/virus_tracker/service/VirusDataService.java
@@ -0,0 +1,26 @@
+package de.virus_tracker.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class VirusDataService {
+    private final static String VIRUS_DATASOURCE_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv";
+
+    // fetch the data
+    @PostConstruct
+    public void fetchVirusData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATASOURCE_URL))
+                .build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.body());
+    }
+}
