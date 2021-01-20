aa9067267479ee4e84047ef8ceebcd7c5472e461
Ayrat <ayrat1@mail.ru>
2021-01-20 21:37:00 +0300

    init project

---
+++ src/main/java/de/virus_tracker/VirusTrackerApplication.java
@@ -0,0 +1,13 @@
+package de.virus_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirusTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/virus_tracker/VirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.virus_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
